61018b54e70bcf7d19bae2cf06ae68d60250bd24
Beth Hansen and Halle Tempest <beth@>
2017-05-10 16:55:03 -0700

    add math random, output result

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 //Business Logic
 
+function PigDice(player, roll, turnScore, playerScore) {
+  this.player = player;
+  this.roll = roll;
+  this.turnScore = turnScore
+  this.playerScore = playerScore
 
-
-
-
-
-
-
-
-
-
+}
+PigDice.prototype.roll = function(){
+  return 
+}
 
 //UI Logic
 
 $(function() {
-  $("form#roll-dice").submit(function(event){
+  $("#rollDice").click(function(event){
     event.preventDefault();
+console.log("button clicked")
+    var die1 = Math.floor ((Math.random() * 6) + 1);
+    var die2 = Math.floor ((Math.random() * 6) + 1);
+
+    var result = die1 + die2;
+
+    $("#output").append(result);
+    // console.log(result);
+});
+});
