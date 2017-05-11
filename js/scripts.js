1e805e72cfe8371392035ea6a0134080dfddd9a9
Beth Hansen and Halle Tempest <halle@>
2017-05-11 10:12:44 -0700

    added business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
 //Business Logic
 
-function PigDice(player, roll, turnScore, playerScore) {
+function Player(player, turnScore, playerScore) {
   this.player = player;
-  this.roll = roll;
-  this.turnScore = turnScore
-  this.playerScore = playerScore
+  this.turnScore = 0;
+  this.playerScore = 0;
+}
 
+Player.prototype.roll = function(){
+  // get a random number
+  var randNum = Math.floor ((Math.random() * 6) + 1);
+
+  this.turnScore = this.turnScore + randNum;
+  // add that random number to current turn score
+
+  return randNum;
 }
-PigDice.prototype.roll = function(){
-  return 
-}
+
+// somePlayer.roll()
+  // return a random number (what they rolled)
+  // add that roll score to turn score
+
 
 //UI Logic
 
 $(function() {
+
+  var player1 = new Player(args...)
+
   $("#rollDice").click(function(event){
+
     event.preventDefault();
-console.log("button clicked")
-    var die1 = Math.floor ((Math.random() * 6) + 1);
-    var die2 = Math.floor ((Math.random() * 6) + 1);
 
-    var result = die1 + die2;
 
-    $("#output").append(result);
+    $("#output").text(player1.roll);
     // console.log(result);
 });
 });
