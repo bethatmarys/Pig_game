b0f376c957025374d1834d411c77c0430cac6042
Beth Hansen and Halle Tempest <halle@>
2017-05-11 16:23:50 -0700

    added back end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,28 @@ function Player(player, turnScore, totalScore) {
 Player.prototype.roll = function(){
   // get a random number
   var randNum = Math.floor ((Math.random() * 6) + 1);
-  //this.turnScore = 0;
+  this.turnScore = 0;
 
   if (randNum === 1){
-    alert("You rolled a 1. Pass the dice!")
+    return ("1")
+    // alert("You rolled a 1. Pass the dice!")
+    // this.turnScore= 0;
   } else if(randNum !== 1){
     this.turnScore += randNum;
-console.log("hi");
+    this.turnScore = this.turnScore + randNum
+    // this.totalScore = this.turnScore
   return randNum;
 
   }
   // add that random number to current turn score
+}
 
+Player.prototype.hold = function(){
+  // adding the turnscore to the totalScore
 
+  this.totalScore = this.totalScore + this.turnScore;
+
+  return totalScore;
 }
 
 // somePlayer.roll()
@@ -32,23 +41,25 @@ console.log("hi");
 //UI Logic
 
 $(function() {
-// event.preventDefault();
+
 
 
   var player1 = new Player("Player 1", 0, 0)
   var player2 = new Player("Player 2", 0, 0)
-  // var turnScore = 0
+
 
 
   $("#rollDice").click(function(){
 
-    var randNum = player1.roll();
-    $("#output").text(randNum);
+    var rollResult = player1.roll();
+
+    $("#output").text(rollResult);
+
     $(".turnScore").text(player1.turnScore);
     console.log (player1.roll)
 
-    $("#results").show();
-     $(".totalScore").text();
-    // console.log(result);
+    $(".totalScore1").text(player1.totalScore);
+    $(".totalScore2").text(player2.totalScore);
+
 });
 });
